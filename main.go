04431c23cb973864217995a558c4b0bb52f5a8aa
Liam Conrad <lconrad@zendesk.com>
2018-06-21 08:52:15 -0500

    init main

---
+++ main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/labstack/echo"
+	"github.com/labstack/echo/middleware"
+)
+
+const (
+	address = ""
+	port = "3500"
+)
+
+func main() {
+	srv := echo.New()
+
+	// Middleware
+	srv.Use(middleware.Logger())
+	srv.Use(middleware.Recover())
+
+	// Routes
+	srv.Any("/", teapot)
+
+	srv.Logger.Fatal(srv.Start(address + ":" + port))
+}
+
+func teapot(c echo.Context) error {
+	return c.JSON(http.StatusTeapot, "{\"status\":\"error\",\"msg\":\"I'm a teapot.\"}")
+}
